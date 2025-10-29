d0f4f8056759ee42f6aec182c3216ccaeb5fa816
vnsslprasanna-garimella <gvnsslprasanna@gmail.com>
2025-10-29 21:13:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Start Coding - Java/start-coding-java.java
@@ -0,0 +1,12 @@
+class Geeks {
+
+    // Function to print hello
+    public static void main(String[] args){
+        printHello();
+    }
+    public static void printHello() {
+System.out.println("Hello World");
+        // Your code here
+        
+    }
+}
