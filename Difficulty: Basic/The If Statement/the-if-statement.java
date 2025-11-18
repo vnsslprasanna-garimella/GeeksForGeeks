073805a4c28eadec20afa493e8664476b8d26af0
vnsslprasanna-garimella <gvnsslprasanna@gmail.com>
2025-11-18 22:05:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/The If Statement/the-if-statement.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static void utility(int Number) {
+        // Write your if statement below
+        if(Number>100)
+        System.out.println("Big");
+        // Write your if statement above
+        System.out.println("Number");
+    }
+}
+
