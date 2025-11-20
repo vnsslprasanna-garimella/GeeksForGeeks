d2d95e831259e1249bbc2403f9d4dabe9b71353b
vnsslprasanna-garimella <gvnsslprasanna@gmail.com>
2025-11-20 22:13:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Input Integer/input-integer.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) {
+        // Implement Scanner Class
+        //  Take input
+        //  Add 10 and Print
+         Scanner sc  = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n+10);
+    }
+}
