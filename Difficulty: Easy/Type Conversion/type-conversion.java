6d878944a13e2d58fd7560097a177d0affa7077b
vnsslprasanna-garimella <gvnsslprasanna@gmail.com>
2025-11-29 21:26:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Type Conversion/type-conversion.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        double d = sc.nextDouble();
+        // Typecast and print
+          int a = (int)d;
+        System.out.print(a);
+    }
+}
