6bfa85f588501c14eb9d7977fcc4ae43418abcac
vnsslprasanna-garimella <gvnsslprasanna@gmail.com>
2025-12-13 22:47:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/String Functions I - Python/string-functions-i-python.py
@@ -0,0 +1,15 @@
+def trim(str):
+    return str.strip()
+
+
+def exists(str, x):
+    return str.find(x)
+
+
+def titleIt(str):
+    return str.title()
+
+
+def casesSwap(str):
+    return str.swapcase()
+
